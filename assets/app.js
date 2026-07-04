a863b5fa6663ad169e9765ef57347846ce96b674
Giv3ntofly <dg.pasqualini@gmail.com>
2026-07-04 19:20:04 +0200

    Corregge selezione carrello desktop

--- assets/app.js
+++ assets/app.js
@@ -1558,7 +1558,7 @@ function escapeHtml(value) {
 function registerServiceWorker() {
   if (!("serviceWorker" in navigator)) return;
   if (!window.isSecureContext && location.hostname !== "localhost" && location.hostname !== "127.0.0.1") return;
-  navigator.serviceWorker.register("./sw.js?v=20260704-9")
+  navigator.serviceWorker.register("./sw.js?v=20260704-10")
     .then(registration => registration.update())
     .catch(() => {});
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nettuno-catalogo-2026-v16";
+const CACHE_NAME = "nettuno-catalogo-2026-v17";
 
 const CORE_ASSETS = [
   "./",
